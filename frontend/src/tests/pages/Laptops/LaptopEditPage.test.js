d11cef9de5d1cd104c58c083821d824ee69995be
Arik <arikcheslog@gmail.com>
2023-05-18 16:03:35 -0700

    ac - mock login check

--- frontend/src/tests/pages/Laptops/LaptopEditPage.test.js
+++ frontend/src/tests/pages/Laptops/LaptopEditPage.test.js
@@ -4,6 +4,10 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import mockConsole from "jest-mock-console";
 import { laptopFixtures } from "fixtures/laptopFixtures";
+import { apiCurrentUserFixtures }  from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import axios from "axios";
+import AxiosMockAdapter from "axios-mock-adapter";
 
 const mockNavigate = jest.fn();
 
@@ -39,8 +43,11 @@ jest.mock('main/utils/laptopUtils', () => {
 
 describe("LaptopEditPage tests", () => {
 
-    const queryClient = new QueryClient();
+    const axiosMock =new AxiosMockAdapter(axios);
+    axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither); 
 
+    const queryClient = new QueryClient();
     test("renders without crashing", () => {
         render(
             <QueryClientProvider client={queryClient}>
