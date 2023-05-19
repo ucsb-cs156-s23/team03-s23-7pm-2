ad4398b76b3ba18253af483b1cf7308b38241561
Arik <arikcheslog@gmail.com>
2023-05-18 19:40:50 -0700

    ac - add laptop page stories

---
+++ frontend/src/stories/pages/Laptops/LaptopCreatePage.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import LaptopCreatePage from "main/pages/Laptops/LaptopCreatePage";
+
+export default {
+    title: 'pages/Laptops/LaptopCreatePage',
+    component: LaptopCreatePage
+};
+
+const Template = () => <LaptopCreatePage />;
+
+export const Default = Template.bind({});
+
+
+
+
---
+++ frontend/src/stories/pages/Laptops/LaptopIndexPage.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import LaptopIndexPage from "main/pages/Laptops/LaptopIndexPage";
+
+export default {
+    title: 'pages/Laptops/LaptopIndexPage',
+    component: LaptopIndexPage
+};
+
+const Template = () => <LaptopIndexPage />;
+
+export const Default = Template.bind({});
+
+
+
+
