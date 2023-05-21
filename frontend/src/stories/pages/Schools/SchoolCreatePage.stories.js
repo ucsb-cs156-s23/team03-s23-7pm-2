460c7234f4d20a22ca6aa636aa22863e637e5061
Maya Ma <yema@cs.ucsb.edu>
2023-05-20 22:34:43 -0700

    mm-added stories of school pages

---
+++ frontend/src/stories/pages/Schools/SchoolCreatePage.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import SchoolCreatePage from "main/pages/Schools/SchoolCreatePage";
+
+export default {
+    title: 'pages/Schools/SchoolCreatePage',
+    component: SchoolCreatePage
+};
+
+const Template = () => <SchoolCreatePage />;
+
+export const Default = Template.bind({});
+
+
+
+
