29b60202346b32e7e8fadcc0bcb08b4d2059d8c3
Maya Ma <yema@cs.ucsb.edu>
2023-05-22 11:47:46 -0700

    mm-convertSchoolJavaBackend

--- frontend/src/main/pages/Schools/SchoolCreatePage.js
+++ frontend/src/main/pages/Schools/SchoolCreatePage.js
@@ -1,18 +1,41 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 import SchoolForm from "main/components/Schools/SchoolForm";
-import { useNavigate } from 'react-router-dom'
-import { schoolUtils } from 'main/utils/schoolUtils';
+import { Navigate } from 'react-router-dom'
+import { useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
 export default function SchoolCreatePage() {
+  const objectToAxiosParams = (school) => ({
+    url: "/api/schools/post",
+    method: "POST",
+    params: {
+      name: school.name,
+      cpu: school.cpu,
+      gpu: school.gpu,
+      description: school.description
+    }
+  });
 
-  let navigate = useNavigate(); 
+  const onSuccess = (school) => {
+    toast(`New school Created - id: ${school.id} name: ${school.name}`);
+  }
 
-  const onSubmit = async (school) => {
-    const createdSchool = schoolUtils.add(school);
-    console.log("createdSchool: " + JSON.stringify(createdSchool));
-    navigate("/schools/list");;
-  }  
+  const mutation = useBackendMutation(
+    objectToAxiosParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    ["/api/schools/all"]
+  );
 
+  const { isSuccess } = mutation
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  }
+
+  if (isSuccess) {
+    return <Navigate to="/schools/list" />
+  }
   return (
     <BasicLayout>
       <div className="pt-2">
